e0ed98b153fd17352c4561b518eca8fbec0ce365
Mohit Ande <andemohit@gmail.com>
2020-07-24 19:12:08 +0530

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Players, teamA, teamB } from './players';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,129 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cricketGame';
+  /** Cricket Team - A */
+  public teamA: Players[] = teamA;
+
+  /** Cricket Team - B */
+  public teamB: Players[] = teamB;
+
+  /** Current battig team */
+  public battingTeam = 'Team - A';
+
+  /** Present player batting */
+  public battingPlayer: string;
+
+  /** Current bowling team */
+  public bowlingTeam = 'Team - B';
+
+  /** Present bowling player */
+  public bowlingPlayer: string;
+
+  /** Number of overs */
+  public overs: number;
+
+  /** Total number of balls */
+  public totalBalls: number;
+
+  public gameStatus: string;
+
+  public innings: number;
+
+  private startGame: number;
+
+  private randomScore = [0, 1, 2, 3, 4, 5, 6, 'wd', 'nb', 'out'];
+
+  constructor() {
+    this.battingPlayer = this.teamA[0].player;
+    this.bowlingPlayer = this.teamB[0].player;
+    this.overs = 0;
+    this.totalBalls = 0;
+    this.gameStatus = 'Start';
+    this.innings = 1;
+    this.startGame = 0;
+  }
+
+  /** Start the game */
+  public bowl(): void {
+    let startGame: number;
+    let batsmanArr = 0;
+    let bowlArr = 0;
+    // this.gameStatus === 'Start' ? this.gameStatus = 'Stop' : this.gameStatus = 'Start';
+    if (this.gameStatus === 'Start') {
+      this.gameStatus = 'Stop';
+    } else {
+      this.gameStatus = 'Start';
+      // clearInterval(startGame);
+    }
+    if (this.overs <= 20.0 && this.gameStatus === 'Stop') {
+      startGame = setInterval(() => {
+
+        // Overs increment
+        if (this.checkDec()) {
+          this.overs += .4;
+        }
+        this.overs = this.overs + 0.1;
+
+        // gets the bowler by overs
+        if (bowlArr < 9 && Math.floor(this.overs) < 9) {
+          bowlArr = Math.floor(this.overs);
+        } else if (bowlArr === 9) {
+          bowlArr = 0;
+        }
+        this.bowlingPlayer = this.teamB[bowlArr].player;
+        bowlArr += 1;
+
+        // gets the batsman
+        if (batsmanArr === 10) {
+          // match end
+        } else {
+          if (this.teamA[batsmanArr].battingStats.wicketBy.length > 0) {
+            batsmanArr += 1;
+          }
+          this.battingPlayer = this.teamA[batsmanArr].player;
+          const randomScore = this.randomScore[Math.floor(Math.random() * this.randomScore.length)];
+          if (typeof(randomScore) === 'number') {
+            this.teamA[batsmanArr].battingStats.bowls += 1; // number of balls played by the player
+            switch (randomScore) {
+              case 4:
+                this.teamA[batsmanArr].battingStats.fours += 1; // number of 4s
+                this.teamA[batsmanArr].battingStats.runs += randomScore;
+                break;
+              case 6:
+                this.teamA[batsmanArr].battingStats.sixs += 1;  // number of 6s
+                this.teamA[batsmanArr].battingStats.runs += randomScore;
+                break;
+              default:
+                this.teamA[batsmanArr].battingStats.runs += randomScore;
+                break;
+            }
+          } else {
+            switch (randomScore) {
+              case 'wd':
+                this.teamA[batsmanArr].battingStats.runs += 1;
+                break;
+              case 'nb':
+                this.teamA[batsmanArr].battingStats.runs += 0;
+                break;
+              case 'out':
+                this.teamA[batsmanArr].battingStats.wicketBy = this.bowlingPlayer;
+            }
+          }
+        }
+
+        // stop if the game reaches to 20 overs;
+        if (Math.floor(this.overs) === 20) {
+          clearInterval(startGame);
+        }
+      }, 1000);
+    } else {
+      // Match end
+      console.log('second innn');
+    }
+  }
+
+  /** Checking the decimal number */
+  private checkDec(): boolean {
+    return JSON.parse(this.overs.toFixed(1).split('.')[1]) > 4 ? true : false;
+  }
 }
---
+++ src/app/players.ts
@@ -0,0 +1,516 @@
+export interface Players {
+    player: string;
+    battingStats: BattingStats;
+    bowlingStats: BowlingStats;
+}
+
+export interface BattingStats {
+    runs: number;
+    bowls: number;
+    fours: number;
+    sixs: number;
+    wicketBy: string;
+    strikeRate: number;
+}
+
+export interface BowlingStats {
+    overs: number;
+    runs: number;
+    wickets: number;
+    econRate: number;
+    zeros: number;
+    fours: number;
+    sixs: number;
+    wide: number;
+    noBall: number;
+}
+
+export const teamA: Players[] = [
+    {
+        player: 'Harbhajan Singh',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'K. L. Rahul',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Virat Kohli',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Rohit Sharma',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Jadeja',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Pandya',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Yuzvendra',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'MS Dhoni',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Shami',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Agarwal',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Dinesh',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+];
+
+export const teamB: Players[] = [
+    {
+        player: 'Jay',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Mohit',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Asheesh',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Bobby',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Dattu',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Raju',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Rajesh',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Satish',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Kumar',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Ravi',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+    {
+        player: 'Teja',
+        battingStats: {
+            runs: 0,
+            bowls: 0,
+            fours: 0,
+            sixs: 0,
+            wicketBy: '',
+            strikeRate: 0
+        },
+        bowlingStats: {
+            overs: 0,
+            runs: 0,
+            wickets: 0,
+            econRate: 0,
+            zeros: 0,
+            fours: 0,
+            sixs: 0,
+            wide: 0,
+            noBall: 0,
+        }
+    },
+];
